dccdb01cd2e4d3e5af2c3c1f1410d40b16226955
ruscuuk <you@example.com>
2022-08-15 17:24:05 +0300

    C# Başlangıç Çalışmaları.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Adınız Nedir ?");
+
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Merhaba" + " " +  name + " " + "Hoşgeldin !" );
+
+
+
